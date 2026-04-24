d8c9b7dd56f46878f593f092c15693781e0228e1
tnit0589-people <tnit0589@gmail.com>
2026-04-24 14:47:57 +0800

    CROS accept vercel.app

--- backend/submission/submit_data.php
+++ backend/submission/submit_data.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://job-submission-system.vercel.app");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
